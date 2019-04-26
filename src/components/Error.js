b2faf6ccc66be273f95c52efd8f30791ed600aa1
victoria-huang <vctrah@gmail.com>
2019-04-26 15:06:34 -0400

    move images

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = (props) => {
     return ( 
         <div>       
-            <img alt='error' height='12px' width='12px' style={{ paddingTop: '2px', marginRight: '2px' }} src={require('../assets/images/ban-sign.png')} />
+            <img alt='error' height='12px' width='12px' style={{ paddingTop: '2px', marginRight: '2px' }} src='ban-sign.png' />
             { props.message }
         </div>
     )
