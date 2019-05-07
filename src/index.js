efb50abfca4b8a45b3842967c9747396d5057782
victoria-huang <vctrah@gmail.com>
2019-05-07 10:03:42 -0400

    add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css'
@@ -9,9 +10,11 @@ import rootReducer from './reducers'
 const store = createStore(rootReducer)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>, 
     document.getElementById('root')
 )
 
