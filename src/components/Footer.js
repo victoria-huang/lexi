fc48b1daac0dd99a9a68220de1779734488b0f5e
victoria-huang <vctrah@gmail.com>
2019-06-05 11:00:04 -0400

    add personal links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaMedium, FaEnvelopeSquare } from 'react-icons/fa'
 
 const Footer = () => (
     <div 
@@ -9,8 +10,23 @@ const Footer = () => (
         }}
     >
         <span>lexi, &#169; 2019</span>
-        <span>created by victoria huang</span>
-        <span>links here</span>
+        <span>
+            created by &nbsp;
+            <a href="https://victoriahuang.me" target="_blank" rel="noopener noreferrer">
+                victoria huang
+            </a>
+        </span>
+        <span className='flex'>
+            <a href="https://github.com/victoria-huang/lexi" target="_blank" rel="noopener noreferrer" style={{ marginRight: '1vh' }}>
+                <FaGithub size={ 15 } />
+            </a>
+            <a href="https://www.linkedin.com/in/huang-victoria/" target="_blank" rel="noopener noreferrer" style={{ marginRight: '1vh' }}>
+                <FaLinkedin size={ 15 } />
+            </a>
+            <a href="mailto:hello@victoriahuang.me">
+                <FaEnvelopeSquare size={ 15 } />
+            </a>
+        </span>
     </div>
 )
 
